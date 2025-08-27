c8c7e7913609a0dd544c98230866b5adecbbc94f
Ricardo Dias <contato@ricardopedias.com.br>
2025-08-27 18:55:19 -0300

    Adicionado suporte a partículas

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if ! [ -d "/usr/share/slidesmaker" ]; then
     sudo mkdir -p /usr/share/slidesmaker
 fi
 
-sudo cp -rf ./assets/* /usr/share/slidesmaker/
+sudo cp -r ./assets/* /usr/share/slidesmaker/
 
 echo "slidesmaker instalado em /usr/bin/slidesmaker"
 echo "Use o comando 'slidesmaker' para executar o programa"
